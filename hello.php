c4749fcea9ced60242e6b5056a1050af5388d081
earth9743 <earth9743@gmail.com>
2024-07-23 14:22:56 +0900

    mysql 연동 성공

--- hello.php
+++ hello.php
@@ -1,2 +1,21 @@
 <?php
+    $servername = "localhost:3307"; # localhost:portnumber
+    $usernadme = "bigcom"; # 설정한 user명
+    $password = "bigcomlab12!@"; # 설정한 비밀번호
+    $dbname = "sample";
+
+    # mysql과 connection 여부
+    $conn = mysqli_connect(
+        $servername, 
+        $usernadme,
+        $password,
+        $dbname
+    );
+
+    try{
+        $conn;
+        die("연결성공");
+    }catch(Excetpion $e){
+        die("연결오류 : ".mysqli_connect_error());
+    }
 ?>
